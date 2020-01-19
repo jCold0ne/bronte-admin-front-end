5678187fc239fbe1a4242fc1dba90a0171bbfbe7
Luke <lukepierson@hotmail.com>
2020-01-20 10:52:56 +1100

    SignIn page created using material-ui

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import App from "./components/App";
+import "typeface-roboto";
 
 ReactDOM.render(<App />, document.getElementById("root"));

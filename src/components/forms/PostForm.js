6c0ddb9c87a84818381d76cf008669e1b51166c1
Luke <lukepierson@hotmail.com>
2020-01-21 15:16:29 +1100

    converted material-ui components to a class based component

--- src/components/forms/PostForm.js
+++ src/components/forms/PostForm.js
@@ -1,16 +1,32 @@
 import React, { Component } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import axios from "axios";
 
 class PostForm extends Component {
   state = {
     title: "",
-    body: ""
+    body: "",
+    error: null
   };
 
   onInputChange = event => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    const { title, body } = this.state;
+
+    axios
+      .post("http://localhost:3000/posts", { title, body })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   render() {
@@ -39,7 +55,12 @@ class PostForm extends Component {
             shrink: true
           }}
         />
-        <Button variant="contained" color="primary" type="button">
+        <Button
+          variant="contained"
+          color="primary"
+          type="button"
+          onClick={this.onFormSubmit}
+        >
           Create Post
         </Button>
         <Button

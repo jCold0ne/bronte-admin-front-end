0b5330478d47c604304db6866bc20171b962dac6
Ragan Martinez <ragan.martinez@live.com>
2020-01-24 16:25:28 +1100

    Render images from db to Image component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 // import authReducer from "./auth_reducer";
 import postsReducer from "./posts_reducer";
+import imagesReducer from "./images_reducer";
 
 export default combineReducers({
   // auth: authReducer,
-  posts: postsReducer
+  posts: postsReducer,
+  images: imagesReducer
 });

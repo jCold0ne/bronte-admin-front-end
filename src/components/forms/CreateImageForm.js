b3e758c4dd46211887fdc35109ccd060dceb2486
Ragan Martinez <ragan.martinez@live.com>
2020-02-03 23:05:33 +1100

    Setup image thumbnail previews

--- src/components/forms/CreateImageForm.js
+++ src/components/forms/CreateImageForm.js
@@ -12,53 +12,6 @@ import Checkbox from "@material-ui/core/Checkbox";
 import { fetchImages } from "../../actions";
 import Dropzone, { useDropzone } from "react-dropzone";
 
-// const baseStyle = {
-//   flex: 1,
-//   display: "flex",
-//   flexDirection: "column",
-//   alignItems: "center",
-//   padding: "20px",
-//   borderWidth: 2,
-//   borderRadius: 2,
-//   borderColor: "#eeeeee",
-//   borderStyle: "dashed",
-//   backgroundColor: "#fafafa",
-//   color: "#bdbdbd",
-//   outline: "none",
-//   transition: "border .24s ease-in-out"
-// };
-
-// const activeStyle = {
-//   borderColor: "#2196f3"
-// };
-
-// const acceptStyle = {
-//   borderColor: "#00e676"
-// };
-
-// const rejectStyle = {
-//   borderColor: "#ff1744"
-// };
-
-// const {
-//   // acceptedFiles,
-//   // getRootProps,
-//   // getInputProps,
-//   isDragActive,
-//   isDragAccept,
-//   isDragReject
-// } = useDropzone({ accept: "image/*" });
-
-// const style = useMemo(
-//   () => ({
-//     ...baseStyle,
-//     ...(isDragActive ? activeStyle : {}),
-//     ...(isDragAccept ? acceptStyle : {}),
-//     ...(isDragReject ? rejectStyle : {})
-//   }),
-//   [isDragAccept, isDragActive, isDragReject]
-// );
-
 const styles = theme => ({
   button: {
     margin: theme.spacing.unit
@@ -185,15 +138,60 @@ class ImageForm extends Component {
     this.props.handleClose();
   };
 
+  getColor = formState => {
+    if (formState.isDragAccept) {
+      return "#00e676";
+    }
+    if (formState.isDragReject) {
+      return "#ff1744";
+    }
+    if (formState.isDragActive) {
+      return "#2196f3";
+    }
+    return "#eeeeee";
+  };
+
+  getStyles = formState => {
+    return {
+      flex: 1,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      padding: "20px",
+      borderWidth: "2px",
+      borderRadius: "2px",
+      borderColor: this.getColor(formState),
+      borderStyle: "dashed",
+      backgroundColor: "#fafafa",
+      color: "#bdbdbd",
+      outline: "none",
+      transition: "border 0.24s ease-in-out"
+    };
+  };
+
   render() {
     const { files, data } = this.state;
+
     return (
-      <div>
+      <div style={{ overflow: "scroll" }}>
         <form encType="multipart/form-data">
-          <Dropzone onDrop={this.onDrop}>
-            {({ getRootProps, getInputProps }) => (
-              <section className="container">
-                <div {...getRootProps({ className: "dropzone" })}>
+          <Dropzone onDrop={this.onDrop} accept="image/*">
+            {({
+              getRootProps,
+              getInputProps,
+              isDragActive,
+              isDragAccept,
+              isDragReject
+            }) => (
+              <section>
+                <div
+                  {...getRootProps({ className: "dropzone" })}
+                  style={this.getStyles({
+                    isDragActive,
+                    isDragAccept,
+                    isDragReject
+                  })}
+                >
                   <input {...getInputProps()} />
                   <p>Drag 'n' drop some files here, or click to select files</p>
                 </div>
@@ -201,6 +199,38 @@ class ImageForm extends Component {
                   {files.map((file, index) => (
                     <>
                       <p>{file.name}</p>
+                      <div
+                        style={{
+                          display: "inline-flex",
+                          borderRadius: 2,
+                          border: "1px solid #eaeaea",
+                          marginBottom: 8,
+                          marginRight: 8,
+                          width: 100,
+                          height: 100,
+                          padding: 4,
+                          boxSizing: "border-box"
+                        }}
+                        key={file.name}
+                      >
+                        <div
+                          style={{
+                            display: "flex",
+                            minWidth: 0,
+                            overflow: "hidden"
+                          }}
+                        >
+                          <img
+                            src={URL.createObjectURL(file)}
+                            alt={file.name}
+                            style={{
+                              display: "block",
+                              width: "auto",
+                              height: "100%"
+                            }}
+                          />
+                        </div>
+                      </div>
                       <div>
                         <label>Caption</label>
                         <input

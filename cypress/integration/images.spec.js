514a427f034c41436412e82604a3190f425cb8be
Cassie <cassandra.sloan16@gmail.com>
2020-02-04 11:57:30 +1100

    testing stuffs:

--- cypress/integration/images.spec.js
+++ cypress/integration/images.spec.js
@@ -1,7 +1,8 @@
+// P A S S E D
 describe("create image button on image route", () => {
   it("create image button pops up form", () => {
     cy.visit("http://localhost:3000/dashboard/images")
-      .get(".MuiButton-containedPrimary") //find create post button
+      .get(".MuiButton-containedPrimary") //find create image button
       .click() //click
       .get("form"); //find form that pops up after click
   });
